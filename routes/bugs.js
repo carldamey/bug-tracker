34aca928a9bc1c3653f5538b4451ece1377ecc9d
carldamey <carldamey@gmail.com>
2023-11-03 00:09:06 -0700

    fixes embarrassing routing bug that took an undisclosed amount of time to discover

--- routes/bugs.js
+++ routes/bugs.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const passport = require("passport")
 const bugsCtrl = require("../controllers/bugs.js")
 
+
+router.delete("/status", bugsCtrl.delete)
 router.get("/report", bugsCtrl.new)
 router.get("/status", bugsCtrl.showStatus)
 router.get("/show", bugsCtrl.show)
 router.post("/", bugsCtrl.create)
-router.delete("/bugs", bugsCtrl.delete)
 
 module.exports = router
